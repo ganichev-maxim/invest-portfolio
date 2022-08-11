e055c8c64f6d06d19c42c0aaa638f45c0a0f6be2
Ganichev <Ganichev@lanit.ru>
2022-08-11 17:03:43 +0300

    Init Portfolio project

---
+++ invest-portfolio/src/test/java/ru/ganichev/invest/portfolio/InvestPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ganichev.invest.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
